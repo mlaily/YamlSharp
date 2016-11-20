020762ee7fc9ef20de5bd8cddb26cd2963e3cd74
Melvyn Laïly <melvyn.laily@gmail.com>
2016-11-20 15:26:16 +0100

    Move ObjectActivator to its own file

---
+++ YamlSharp/Serialization/ObjectActivator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YamlSharp.Serialization
+{
+    internal class ObjectActivator
+    {
+        Dictionary<Type, Func<object>> activators = new Dictionary<Type, Func<object>>();
+
+        public void Add<T>(Func<object> activator)
+            where T : class
+        {
+            activators.Add(typeof(T), activator);
+        }
+
+        public T Activate<T>() where T : class => (T)Activate(typeof(T));
+
+        public object Activate(Type type)
+        {
+            if (!activators.ContainsKey(type))
+                return Activator.CreateInstance(type);
+            return activators[type].Invoke();
+        }
+    }
+}
--- YamlSharp/Serialization/YamlConstructor.cs
+++ YamlSharp/Serialization/YamlConstructor.cs
@@ -11,27 +11,6 @@ using YamlSharp.Model;
 
 namespace YamlSharp.Serialization
 {
-    internal class ObjectActivator
-    {
-        Dictionary<Type, Func<object>> activators =
-            new Dictionary<Type, Func<object>>();
-        public void Add<T>(Func<object> activator)
-            where T : class
-        {
-            activators.Add(typeof(T), activator);
-        }
-        public T Activate<T>() where T : class
-        {
-            return (T)Activate(typeof(T));
-        }
-        public object Activate(Type type)
-        {
-            if (!activators.ContainsKey(type))
-                return Activator.CreateInstance(type);
-            return activators[type].Invoke();
-        }
-    }
-
     /// <summary>
     /// Construct a YAML node tree that represents a given C# object.
     /// </summary>
