bff07e39602a783a37dc87da9d555d508e2e47a9
Melvyn Laïly <melvyn.laily@gmail.com>
2016-11-27 10:12:19 +0100

    benchmark

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YamlSharp.Model;
+using YamlSharp.Parsing;
 
 namespace ConsoleApplication1
 {
@@ -12,60 +14,91 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var content = @"
+            ////            var content = @"
 
-# dfgdfg
+            ////# dfgdfg
 
- &a [*a]";
+            //// &a [*a]";
 
-            //            var content = @"
-            //? a :
-            //    - b
-            //    -  - c
-            //       - d
-            //";
+            ////            var content = @"
+            ////? a :
+            ////    - b
+            ////    -  - c
+            ////       - d
+            ////";
 
-            //            var content = @"
+            //var content = @"
             //a : &anchor foo
             //c : *anchor
             //b : &anchor bar
             //d : *anchor
             //";
 
-            //            var content = @"
-            //[
-            //""folded to a space,\n\
-            //      to a line feed, \
-            //      or \t \tnon - content""
-            //,
-            //""folded 
-            //to a space,	
+            ////            var content = @"
+            ////[
+            ////""folded to a space,\n\
+            ////      to a line feed, \
+            ////      or \t \tnon - content""
+            ////,
+            ////""folded 
+            ////to a space,	
 
-            //to a line feed, or 	\
-            // \ 	non - content""
-            //]";
+            ////to a line feed, or 	\
+            //// \ 	non - content""
+            ////]";
 
-            //            var content = @"
-            //--- ""he\0lჿFFlo""
-            //";
-            // System.Text.Encoding.UTF8.
-            var x = char.ConvertFromUtf32(0x10FFFF);
-            var y = char.ConvertFromUtf32(0x20);
-            var z = char.ConvertFromUtf32(0x41);
-            var a = char.ConvertFromUtf32(0x2665);
-            string bla = x + y + z + a;
+            ////            var content = @"
+            ////--- ""he\0lჿFFlo""
+            ////";
+            //// System.Text.Encoding.UTF8.
+            //var x = char.ConvertFromUtf32(0x10FFFF);
+            //var y = char.ConvertFromUtf32(0x20);
+            //var z = char.ConvertFromUtf32(0x41);
+            //var a = char.ConvertFromUtf32(0x2665);
+            //string bla = x + y + z + a;
+
+            // string content = "!hel%F4%8F%BF%BFlo%E2%99%A5";
+
+            string content = "[a,[b,c],d]";
+
             var contentNodes = YamlNode.FromYaml(content);
 
-            var back = contentNodes.First().ToYaml();
+            //var back = contentNodes.First().ToYaml();
 
-            using (var reader = new StreamReader(@"D:\Mes Documents\Dev\yaml\yamlreference\.stack-work\install\7282fce5\bin\gros.txt",
-                Encoding.UTF8))
+            //var backContent = YamlNode.FromYaml(back);
+
+            var seq = contentNodes.First() as YamlSequence;
+            var a = seq[0];
+            var bc = seq[1];
+            var d = seq[2];
+
+            string inMemoryString;
+            using (var reader = new StreamReader(@"D:\Mes Documents\Dev\yaml\yamlreference\.stack-work\install\7282fce5\bin\gros.txt", Encoding.UTF8))
             {
-                var nodes = YamlNode.FromYaml(reader);
-                YamlSharp.Serialization.YamlSerializer serializer = new YamlSharp.Serialization.YamlSerializer();
-                var roundTrip = serializer.Serialize(nodes);
+                inMemoryString = reader.ReadToEnd();
             }
 
+            //YamlSharp.Serialization.YamlSerializer serializer = new YamlSharp.Serialization.YamlSerializer();
+            //var roundTrip = serializer.Serialize(nodes);
+
+
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            for (int i = 0; i < 7; i++)
+            {
+                var nodes = YamlNode.FromYaml(inMemoryString, YamlNode.DefaultConfig);
+                yaml = nodes;
+            }
+
+            watch.Stop();
+            Debug.WriteLine(watch.Elapsed);
+            result = (watch.ElapsedMilliseconds / 7).ToString();
+            // around 1500-1550 ms / iteration on my machine
+
         }
+        static string result;
+        static YamlNode[] yaml;
     }
 }
